f736d2813b977dfb621bc3c6c9f04211250a9590
Alyssa G <agrenardo@brooklynsteamcenter.org>
2023-03-27 10:53:01 -0400

    post feed added

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html.jinja")
 
-@app.route("/post")
-def post():
-    return render_template("<p>Current Post</p>")
+connection = pymysql.connect(
+    host='10.100.33.60',
+    user='agrenardo',
+    password='220279616',
+    database='agrenardo_todo',
+    cursorclass=pymysql.cursors.DictCursor,
+    autocommit=True 
+)
 
-@app.route("/hello/<name>")
-def hello(name):
-    return f"<p>Hello, {name}!</p>"
+if __name__=='__main__':
+    app.run(debug=True)
 
-@app.route("/")
-def index():
-    return render_template(
-        "home.html.jinja", 
-    )
+@app.route('/feed')
+def post_feed():
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM `posts` ORDER BY `timestamp`")
+
+    results = cursor.fetchall()
+
+return render_template(
+    "feed.html.jinja",
+    posts = results
+)
