80814c2b8f263a52f279e61952d37cb70952452b
Alyssa G <agrenardo@brooklynsteamcenter.org>
2023-03-29 10:58:58 -0400

    form submit

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ connection = pymysql.connect(
     autocommit=True 
 )
 
-if __name__=='__main__':
-    app.run(debug=True)
+
 
 @app.route('/post')
 def post_feed():
@@ -32,3 +31,26 @@ def post_feed():
     "posts.html.jinja",
     posts = results
 )
+
+@app.route('/sign-in')
+def sign_in():
+    return render_template("sign_in.html.jinja")
+
+
+@app.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method == 'POST':
+       cursor = connection.cursor()
+
+       cursor.execute(""""
+           INSERT INTO `users` (`username`, `email`, `display_name`, `password`, `bio`, `photo`)
+           VALUES (%s, %s, %s, %s, %s, %s)
+       """, [])
+
+       return request.form
+    elif request.method == 'GET':
+        return render_template("sign_up.html.jinja")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
